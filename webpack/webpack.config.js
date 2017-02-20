ec0df29adec4c8a52cb2de2c5b1c23c9245c9bc2
PengchengYuan <yuan.peng.cheng@163.com>
2017-02-20 22:45:00 +0800

    software

---
+++ webpack/webpack.config.js
@@ -0,0 +1,21 @@
+//npm install  下载插件
+var webpack = require('webpack'); //加载webpack模块
+//进行一定的配置
+module.exports = {
+	entry: {
+	  //入口js文件
+	  bundle: './js/entry/entry.js'
+	},
+	output: {
+	  //入口js文件，在经过webpack处理之后，你希望它输出到哪个目录下
+	  path: './js/', //发布目录
+	  filename: '[name].js'
+	}/*,
+	plugins:[
+	  new webpack.ProvidePlugin({
+		    $:"jquery",
+		    jQuery:"jquery",
+		    "window.jQuery":"jquery"
+	  })
+	]*/
+};
